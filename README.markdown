720af41ca652bf0508ef55343902f13860c1772f
Eric DeLabar <edelabar@gmail.com>
2011-12-29 22:48:57 -0500

    Merged in latest html5 boilerplate

--- README.markdown
+++ README.markdown
@@ -1,2 +1,4 @@
 # This is my website
 Check it out at http://ericdelabar.com/ (after it launches of course...)
+
+Requires Jekyll, Rdiscount, Pygments, and [binil/pygments-groovy-lexer](https://github.com/binil/pygments-groovy-lexer)
--- js/plugins.js
+++ js/plugins.js
@@ -1,4 +1,3 @@
-
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
@@ -10,6 +9,15 @@ window.log = function(){
 // make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
-
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+/* http://adactio.com/journal/4470/ */
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  var viewportmeta = document.querySelector('meta[name="viewport"]');
+  if (viewportmeta) {
+    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+    document.body.addEventListener('gesturestart', function() {
+      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+    }, false);
+  }
+}
